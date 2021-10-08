40ba939312fe07aed88244adf9704dba386f2857
BLOCKMIR ParkDongGyu <djqjqjfu@Naver.com>
2021-10-08 17:52:52 +0900

    바닐라 자바스크립트

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const age = parseInt(prompt("how old are you?"));
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
 
-if(isNaN(age)){
-    console.log("No Number")
-}else if(age <= 19){
-    console.log("No")
-}else if(age >= 19){
-    console.log("Yes")
+function onLoginBtnClick(){
+    const username = loginInput.value;
+    console.log(username)
 }
+
+loginButton.addEventListener("click",onLoginBtnClick);
